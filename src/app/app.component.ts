e77befef390c087bd6041ae094321ae12eb9c9f1
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-10-31 12:30:58 +0100

    Modified component app ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Artist Directory</h1>
-  <p>Hello</p>`,
+  template: `
+  <div class="row justify-content-center">
+  <div class="card mt-sm-3 col-md-8">
+    <div class="card-body">
+      <h3 class="mb-0">Artist Directory</h3>
+      <div class="form-group">
+        <div class="form-label"></div>
+        <input class="form-control mt-2" type="text">
+      </div><!-- form-group -->
+    </div><!-- card-body -->
+   </div><!-- card -->
+  </div><!-- row -->  
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
