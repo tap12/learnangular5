1ad172535e8ece96dc8e00b9847a96ecd8b7f439
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-10-31 12:05:06 +0100

    Added some changes in app.component.ts - modified it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Artist Directory</h1>
+  <p>Hello</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
