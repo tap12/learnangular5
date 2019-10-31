23a8ceb7bdf57dafbc01f5f992fe9da40a9f7744
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-10-31 21:58:30 +0100

    created subcomponents

---
+++ src/app/artist-items/artist-items.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArtistItemsComponent } from './artist-items.component';
+
+describe('ArtistItemsComponent', () => {
+  let component: ArtistItemsComponent;
+  let fixture: ComponentFixture<ArtistItemsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ArtistItemsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistItemsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
